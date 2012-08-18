5728593de574e11c4f7bc5d0e239afc84fb33460
Ajuna <ajunasm@gmail.com>
2012-08-18 21:37:53 +0530

    First commit

---
+++ string1_A.py
@@ -0,0 +1,12 @@
+print "Enter the donuts in your hand??"
+count=input()
+
+def donuts(count):
+        if count<10:
+                print "Number of donuts:",count
+        else:
+                print "Number of donuts:many"
+
+
+donuts(count)
+
