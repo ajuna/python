4a48d493df3886283535d823a26d5a430e982570
ajuna <ajunasm@gmail.com>
2012-08-19 09:12:50 +0530

    Update list2_D.py

--- list2_D.py
+++ list2_D.py
@@ -2,11 +2,9 @@ print "Enter a list of numbers:"
 nums=input()
 
 def remove_adjacent(nums):
-	#nums=nums.sort()
 	x=[]
 	i=0
 	n=len(nums)
-	#for num in nums:
 	while(i<n):
 		if(len(x)==0 or nums[i]!=x[-1]):
 			x.append(nums[i])
